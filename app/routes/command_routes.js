d265e5c0f99c81c4e719729b4e48c244f80a466d
Sid <siddharthkrish@gmail.com>
2019-05-28 11:43:03 +0800

    some formatting

--- app/routes/command_routes.js
+++ app/routes/command_routes.js
@@ -1,6 +1,6 @@
 module.exports = function (app, db) {
   app.post('/command', (req, res) => { // You'll create your note here.
     console.log( req.body )
-    res.send('new command: ' + req.body.opt)
+    res.send(`new command: ${req.body}`)
   });
 };
