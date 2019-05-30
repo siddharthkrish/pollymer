281d2cbcb69fd663e5f173420ec1b104ed5d88df
Siddharth Krish <siddharthkrish@gmail.com>
2019-05-30 08:54:01 +0800

    updated the message

--- app/routes/command_routes.js
+++ app/routes/command_routes.js
@@ -1,6 +1,6 @@
 module.exports = function (app, db) {
   app.post('/command', (req, res) => { // You'll create your note here.
     console.log( req.body )
-    res.send(`new command: ${req.body}`)
+    res.send(`will be sending this poll ${req.body.user_name}, "${req.body.text}"`)
   });
 };
